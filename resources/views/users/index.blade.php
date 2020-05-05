f02e7f86bc4db1219a803d9a776add8cfb8e9916
Filippo Mailli <filippo.mailli@gmail.com>
2020-05-05 18:19:13 +0200

    .1 Route e cose base senza stile

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        @php
+            $users = [
+                [
+                    'name' => '<p>Pippo</p>',
+                    'email' => 'pippo@email.it',
+                    'age' => 30
+                ],
+                [
+                    'name' => '<p>Topolino</p>',
+                    'email' => 'topolino@email.it',
+                    'age' => 50
+                ],
+                [
+                    'name' => '<p>Minnie</p>',
+                    'email' => 'minnie@email.it',
+                    'age' => 25
+                ]
+            ];
+        @endphp
+
+        <main>
+            <a href="{{route('home')}}">Home</a>
+            {{-- QUESTO E' UN COMMENTO CHE NON VERRA' VISUALIZZATO --}}
+            @foreach ($users as $user)
+
+            @if ($loop->first)
+                {{$user['name']}}
+            @endif
+
+                {{--@if ($user['age'] > 30)
+                    <ul>
+                        <li>{!!$user['name']!!}</li>
+                        <li>{{$user['email']}}</li>
+                        <li>{{$user['age']}}</li>
+                    </ul>
+                @endif--}}
+
+            @endforeach
+
+            {{--@forelse ($users as $user)
+                <li>{{ $user['name'] }}</li>
+            @empty
+                <p>No users</p>
+            @endforelse--}}
+        </main>
+    </body>
+</html>
